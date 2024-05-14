f689bbca2de18a90575d727466dccaf8f66a5442
Alex Dewar <a.dewar@imperial.ac.uk>
2024-05-14 11:58:51 +0100

    Create course.zip on the fly with GitHub Action
    Closes #38.

---
+++ code/course/messy.py
@@ -0,0 +1,14 @@
+x = {  'a':37,'b':42,
+'c':927}
+y = 'hello '+       'world'
+class foo  (     object  ):
+   def f    (self   ):
+       z =3
+       return       y **2
+   def g(self, x,
+       y=42
+       ):
+      # pylint: disable=missing-docstring
+      return x--y
+def f  (   a ) :
+   return      37+-a[42-a :  y*3]  # noqa: E203
